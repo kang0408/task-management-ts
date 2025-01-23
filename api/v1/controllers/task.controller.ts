f640204eae01322405972b5bd2114d76044eade0
Kang0408 <trandanhkhang482004@gmail.com>
2025-01-23 15:32:57 +0700

    add models

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -1,4 +1,4 @@
-import Task from "../../../models/task.model";
+import Task from "../models/task.model";
 import { Request, Response } from "express";
 
 export const index = async (req: Request, res: Response) => {
